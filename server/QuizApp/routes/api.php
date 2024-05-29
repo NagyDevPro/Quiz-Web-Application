c968e1b7b46867b71918813e05d57e53169dc9c6
dohaseif2 <dohaseif2@gmail.com>
2024-05-29 14:11:35 +0300

    feat(api): Store question with his choices

--- server/QuizApp/routes/api.php
+++ server/QuizApp/routes/api.php
@@ -1,13 +1,21 @@
 <?php
 
 use App\Http\Controllers\ExamController;
+use App\Http\Controllers\QuestionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::get('/exams',[ExamController::class,'index']);
-Route::post('/exams',[ExamController::class,'store']);
-Route::put('/exams/{id}',[ExamController::class,'update']);
-Route::delete('/exams/{id}',[ExamController::class,'destroy']);
+
+
+Route::get('/exam',[ExamController::class,'index']);
+Route::post('/exam',[ExamController::class,'store']);
+Route::put('/exam/{id}',[ExamController::class,'update']);
+Route::delete('/exam/{id}',[ExamController::class,'destroy']);
+
+
+Route::post('/question/{examId}',[QuestionController::class,'store']);
+Route::put('/question/{id}',[QuestionController::class,'update']);
+Route::delete('/question/{id}',[QuestionController::class,'destroy']);
