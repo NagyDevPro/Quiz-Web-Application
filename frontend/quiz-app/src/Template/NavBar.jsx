c5ceab45d9d4e3116f336ee285ca413dc2a09d4a
Blitz576 <anagy8050@gmail.com>
2024-06-20 09:57:35 +0300

    build(react) : Intial project main dependancies

---
+++ frontend/quiz-app/src/Template/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './NavBar.css';
+import { NavLink } from 'react-bootstrap';
+
+export default function NavBar() {
+  return (
+    <div className="container navBarContainer">
+        <NavLink href='#'>
+        <img className='homeIcon' src="https://cdn-icons-png.flaticon.com/512/2641/2641409.png" alt="" />
+        </NavLink>
+        <div className="mx-2 fs-5 d-flex justify-content-between" style={{ marginTop: "3%" ,width : "35%"}}>  
+        
+        <NavLink href="#" > <i class="bi bi-box-arrow-in-right mx-2 fs-5"></i> Login</NavLink>
+        <NavLink href='#' className='nav-link'>About Us</NavLink>
+        <NavLink href='#' className='nav-link'>Contact Us</NavLink>
+       </div>
+    </div>
+  )
+}
