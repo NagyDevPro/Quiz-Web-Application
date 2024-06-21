78af37c37713121946f3658ba4b108270ad0d3fa
Blitz576 <anagy8050@gmail.com>
2024-06-21 12:03:08 +0300

    feat(react) : Exam api endponints using axios

---
+++ frontend/quiz-app/src/Admin/api/exams-api.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const baseURL = "http://127.0.0.1:8000/api";
+const examsUrl = `${baseURL}/exams`;
+const specificExamUrl = (id) => `${baseURL}/exams/${id}`;
+
+const createExam = (examData, token) =>
+  axios.post(examsUrl, examData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+const editExam = (id, examData, token) =>
+  axios.put(specificExamUrl(id), examData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+const deleteExam = (id, token) =>
+  axios.delete(specificExamUrl(id), {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+const showAllExams = (token) =>
+  axios.get(examsUrl, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+const showSpecificExam = (id, token) =>
+  axios.get(specificExamUrl(id), {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+export { createExam, editExam, deleteExam, showAllExams, showSpecificExam };
